463a9e18c2fc885e3c50e17639676c2fa4755ddf
ihiratch <junya737@gmail.com>
2025-03-30 15:05:02 +0000

    Module for config

---
+++ scripts/module/config_lib.py
@@ -0,0 +1,99 @@
+import torch
+
+
+class CFG:
+    def __init__(self, mode="train", kaggle_notebook=False, debug=True):
+        assert mode in ["train", "inference"], "mode must be 'train' or 'inference'"
+        self.mode = mode
+        self.KAGGLE_NOTEBOOK = kaggle_notebook
+        self.debug = debug
+
+        # ===== Path Settings =====
+        if self.KAGGLE_NOTEBOOK:
+            self.OUTPUT_DIR = ''
+            self.train_datadir = '/kaggle/input/birdclef-2025/train_audio'
+            self.train_csv = '/kaggle/input/birdclef-2025/train.csv'
+            self.test_soundscapes = '/kaggle/input/birdclef-2025/test_soundscapes'
+            self.submission_csv = '/kaggle/input/birdclef-2025/sample_submission.csv'
+            self.taxonomy_csv = '/kaggle/input/birdclef-2025/taxonomy.csv'
+            self.spectrogram_npy = '/kaggle/input/birdclef25-mel-spectrograms/birdclef2025_melspec_5sec_256_256.npy'
+            self.model_path = '/kaggle/input/birdclef25-effnetb0-starter-weight'
+        else:
+            self.OUTPUT_DIR = '../data/result/'
+            self.train_datadir = '../data/raw/train_audio/'
+            self.train_csv = '../data/raw/train.csv'
+            self.test_soundscapes = '../data/raw/test_soundscapes/'
+            self.submission_csv = '../data/raw/sample_submission.csv'
+            self.taxonomy_csv = '../data/raw/taxonomy.csv'
+            self.spectrogram_npy = '../data/processed/mel-spec_0329/birdclef2025_melspec_5sec_256_256.npy'
+            self.MODEL_DIR = "../models/"
+            self.model_path = self.MODEL_DIR
+
+        # ===== Model Settings =====
+        self.model_name = 'efficientnet_b0'
+        self.pretrained = True if mode == "train" else False
+        self.in_channels = 1
+
+        # ===== Audio Settings =====
+        self.FS = 32000
+        self.WINDOW_SIZE = 5.0
+        self.TARGET_DURATION = 5.0
+        self.TARGET_SHAPE = (256, 256)
+        self.N_FFT = 1024
+        self.HOP_LENGTH = 512
+        self.N_MELS = 128
+        self.FMIN = 50
+        self.FMAX = 14000
+
+        # ===== Device =====
+        
+
+        # ===== Training Mode =====
+        if mode == "train":
+            self.seed = 42
+            self.apex = False
+            self.print_freq = 100
+            self.num_workers = 2
+
+            self.LOAD_DATA = True
+            self.epochs = 10
+            self.batch_size = 32
+            self.criterion = 'BCEWithLogitsLoss'
+
+            self.n_fold = 5
+            self.selected_folds = [0, 1, 2, 3, 4]
+
+            self.optimizer = 'AdamW'
+            self.lr = 5e-4
+            self.weight_decay = 1e-5
+            self.scheduler = 'CosineAnnealingLR'
+            self.min_lr = 1e-6
+            self.T_max = self.epochs
+
+            self.aug_prob = 0.5
+            self.mixup_alpha = 0.5
+            
+            self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+            if self.debug:
+                self.epochs = 2
+                self.selected_folds = [0]
+
+        # ===== Inference Mode =====
+        elif mode == "inference":
+            self.batch_size = 16
+            self.use_tta = False
+            self.tta_count = 3
+            self.threshold = 0.5
+
+            self.use_specific_folds = False
+            self.folds = [0, 1]  # Used only if use_specific_folds is True
+
+            self.debug_count = 3
+            
+            self.device = "cpu"
+            
+    def update_debug_settings(self):
+        if self.debug:
+            self.epochs = 2
+            self.selected_folds = [0]
