12232416d4d859181b18e0929a5aecacd9bb98a0
ihiratch <junya737@gmail.com>
2025-03-30 13:53:05 +0000

    Modules

---
+++ scripts/module/learning.py
@@ -0,0 +1,69 @@
+import torch
+from torch import nn, optim
+from torch.optim import lr_scheduler
+
+def get_optimizer(model, cfg):
+  
+    if cfg.optimizer == 'Adam':
+        optimizer = optim.Adam(
+            model.parameters(),
+            lr=cfg.lr,
+            weight_decay=cfg.weight_decay
+        )
+    elif cfg.optimizer == 'AdamW':
+        optimizer = optim.AdamW(
+            model.parameters(),
+            lr=cfg.lr,
+            weight_decay=cfg.weight_decay
+        )
+    elif cfg.optimizer == 'SGD':
+        optimizer = optim.SGD(
+            model.parameters(),
+            lr=cfg.lr,
+            momentum=0.9,
+            weight_decay=cfg.weight_decay
+        )
+    else:
+        raise NotImplementedError(f"Optimizer {cfg.optimizer} not implemented")
+        
+    return optimizer
+
+def get_scheduler(optimizer, cfg):
+   
+    if cfg.scheduler == 'CosineAnnealingLR':
+        scheduler = lr_scheduler.CosineAnnealingLR(
+            optimizer,
+            T_max=cfg.T_max,
+            eta_min=cfg.min_lr
+        )
+    elif cfg.scheduler == 'ReduceLROnPlateau':
+        scheduler = lr_scheduler.ReduceLROnPlateau(
+            optimizer,
+            mode='min',
+            factor=0.5,
+            patience=2,
+            min_lr=cfg.min_lr,
+            verbose=True
+        )
+    elif cfg.scheduler == 'StepLR':
+        scheduler = lr_scheduler.StepLR(
+            optimizer,
+            step_size=cfg.epochs // 3,
+            gamma=0.5
+        )
+    elif cfg.scheduler == 'OneCycleLR':
+        scheduler = None  
+    else:
+        scheduler = None
+        
+    return scheduler
+
+def get_criterion(cfg):
+ 
+    if cfg.criterion == 'BCEWithLogitsLoss':
+        criterion = nn.BCEWithLogitsLoss()
+    else:
+        raise NotImplementedError(f"Criterion {cfg.criterion} not implemented")
+        
+    return criterion
+
